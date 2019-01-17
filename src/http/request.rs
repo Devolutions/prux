464f275e6908b026850478e14b8fad1fe05680c9
spicheaubin <spicheaubin@devolutions.net>
2019-01-17 16:43:20 -0500

    Add the reqwest crate + the get function

---
+++ src/http/request.rs
@@ -0,0 +1,46 @@
+use tokio::prelude::*;
+use std::io::Cursor;
+use std::io;
+use reqwest::r#async::Decoder;
+use std::mem::replace;
+use core::mem;
+use reqwest::r#async::Chunk;
+use hyper::HeaderMap;
+use hyper::header::AUTHORIZATION;
+use serde_json::Value;
+use tokio::prelude::future::FutureResult;
+use reqwest::r#async::{Client, Response};
+use base64::encode;
+
+pub struct HttpRequest {
+    client: Client,
+    headers: HeaderMap
+}
+
+impl HttpRequest {
+    pub fn new(id: &str, password: &str) -> Self{
+        let mut headers = HeaderMap::new();
+        let encoded = encode(&format!("{}:{}", id, password));
+
+        headers.append(AUTHORIZATION, format!("Basic {}", encoded).parse().unwrap());
+
+        HttpRequest{
+            client: Client::new(),
+            headers
+        }
+    }
+
+    pub fn lookup(&self, addr: &str) -> impl Future<Item=Value, Error=()> {
+        let json = |mut res : Response | {
+            let result = res.json::<Value>().map_err(|_|());
+
+            result
+        };
+
+        self.client
+            .get(&format!("https://geoip.maxmind.com/geoip/v2.1/city/{}", addr))
+            .headers(self.headers.clone())
+            .send().map_err(|_| ())
+            .and_then(json)
+    }
+}
