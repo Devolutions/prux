b3a50f31492aabbf26caafd7a4f2818b388189a0
Allan Zhang <6740989+allan2@users.noreply.github.com>
2025-07-30 14:28:08 -0400

    chore: update clap to v4 with derive API
    The settings module is updated. More info is shown in `--help`.
    
    For example, the default value for format is now shown.
    The log level stays lowercase in the config file by using a custom serializer/deserializer.
    
    ```
    Usage: prux [OPTIONS]
    
    Options:
      -c, --config <CONFIGFILE>
      -l, --level <LOGLEVEL>
      -p, --port <LISTENER_PORT>
      -u, --uri <SERVER_URI>
      -i, --maxmindid <MAXMIND_ID>
      -s, --maxmindpass <MAXMIND_PASSWORD>
          --save-config <SAVE_CONFIG>
          --format <FORMAT>                 [default: TOML] [possible values: TOML, YAML, JSON]
          --show-config
      -h, --help                            Print help
      -V, --version                         Print version
    ```

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ async fn main() -> io::Result<()> {
     let config = settings::Settings::load().expect("Configuration errors are fatal");
 
     let mut builder = Builder::new();
-    builder.filter(None, config.level_filter());
+    builder.filter(None, config.loglevel);
     builder.filter(Some("tokio_io"), LevelFilter::Off);
     builder.filter(Some("tokio_core"), LevelFilter::Off);
     builder.filter(Some("tokio_reactor"), LevelFilter::Off);
